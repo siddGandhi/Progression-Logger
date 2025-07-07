8e7a3b0d944b79696744471946358c9a6b6cb149
sidd <sidd@Mac.bbrouter>
2025-07-07 20:17:36 +0530

    added view and mvmnts.js

---
+++ movements.js
@@ -0,0 +1,12 @@
+export const officialMovements = [
+  "Dumbbell Bench Press",
+  "Barbell Bench Press",
+  "Squat",
+  "Deadlift",
+  "Romanian Deadlift",
+  "Pull-ups",
+  "Overhead Press",
+  "Incline Bench Press",
+  "Bent Over Row",
+  "Lat Pulldown",
+];
